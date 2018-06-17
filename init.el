bbf4f87ffb4290e47d93cfa3e393e890e16f8a04
ShawnGeller <36929468+ShawnGeller@users.noreply.github.com>
2018-06-16 18:45:10 -0600

    Moved stuff around to put it all in one file

--- init.el
+++ init.el
@@ -1,29 +1,29 @@
 ;; Shawn's init.el. First load package libraries
 
-(require 'package) ;; You might already have this line
+(require 'package)
+
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
 		    (not (gnutls-available-p))))
        (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
   (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t))
+
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
 (add-to-list 'package-archives
 	                  '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 
-(package-initialize) ;; You might already have this line
+(package-initialize)
 
 (setq inhibit-startup-message t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; company mode
-(add-hook 'after-init-hook 'global-company-mode)
-(global-set-key (kbd "C-c C-i") 'counsel-company)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; evil mode
@@ -32,12 +32,7 @@
 (setq evil-want-C-i-jump nil)
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 (setq evil-want-C-u-scroll t)
-
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-key "e" 'find-file)
-(evil-leader/set-key-for-mode 'python-mode "s" 'elpy-shell-send-region-or-buffer) 
-(evil-leader/set-key-for-mode 'python-mode "d" 'elpy-goto-definition) 
+(setq evil-want-C-d-scroll t)
 
 (require 'evil)
 (evil-mode 1)
@@ -52,6 +47,13 @@
 (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
 
 (global-linum-mode t)
+
+(require 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-key "e" 'find-file)
+(evil-leader/set-key-for-mode 'python-mode "s" 'elpy-shell-send-region-or-buffer) 
+(evil-leader/set-key-for-mode 'python-mode "d" 'elpy-goto-definition) 
+
 
 ;; esc quits
 (defun minibuffer-keyboard-quit ()
@@ -114,6 +116,12 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  '(("i" ivy-copy-to-buffer-action "insert")
    ("y" ivy-yank-action "yank")))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; company mode
+
+(add-hook 'after-init-hook 'global-company-mode)
+(global-set-key (kbd "C-c C-i") 'counsel-company)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; magit
 
@@ -140,42 +148,42 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
      (setq-local yas/trigger-key [tab])
 	(define-key yas/keymap [tab] 'yas/next-field-or-maybe-expand)))
 
-(defun org-completion-symbols ()
-  (when (looking-back "=[a-zA-Z]+")
-    (let (cands)
-      (save-match-data
-	(save-excursion
-	  (goto-char (point-min))
-	  (while (re-search-forward "=\\([a-zA-Z]+\\)=" nil t)
-	    (cl-pushnew
-	     (match-string-no-properties 0) cands :test 'equal))
-	  cands))
-      (when cands
-	(list (match-beginning 0) (match-end 0) cands)))))
+;; (defun org-completion-symbols ()
+;;   (when (looking-back "=[a-zA-Z]+")
+;;     (let (cands)
+;;       (save-match-data
+;; 	(save-excursion
+;; 	  (goto-char (point-min))
+;; 	  (while (re-search-forward "=\\([a-zA-Z]+\\)=" nil t)
+;; 	    (cl-pushnew
+;; 	     (match-string-no-properties 0) cands :test 'equal))
+;; 	  cands))
+;;       (when cands
+;; 	(list (match-beginning 0) (match-end 0) cands)))))
 
-(defun ora-cap-filesystem ()
-  (let (path)
-    (when (setq path (ffap-string-at-point))
-      (let ((compl
-	     (all-completions path #'read-file-name-internal)))
-	(when compl
-	  (let ((offset (ivy-completion-common-length (car compl))))
-	                (list (- (point) offset) (point) compl)))))))
+;; (defun ora-cap-filesystem ()
+;;   (let (path)
+;;     (when (setq path (ffap-string-at-point))
+;;       (let ((compl
+;; 	     (all-completions path #'read-file-name-internal)))
+;; 	(when compl
+;; 	  (let ((offset (ivy-completion-common-length (car compl))))
+;; 	                (list (- (point) offset) (point) compl)))))))
 
-(setq completion-at-point-functions
-      '(org-completion-symbols
-	ora-cap-filesystem
-	org-completion-refs))
+;; (setq completion-at-point-functions
+;;       '(org-completion-symbols
+;; 	ora-cap-filesystem
+;; 	org-completion-refs))
 
 
-(setq org-capture-templates
-  (quote (("t" "todo" entry (file (concat org-directory "/gtd.org"))
-	   "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-	  ("n" "note" entry (file (concat org-directory "/gtd.org"))
-	   "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-	  ("j" "Journal" entry (file+datetree (concat org-directory "/diary.org"))
-	   "* %?\n%U\n" :clock-in t :clock-resume t)
-	  )))
+;; (setq org-capture-templates
+;;   (quote (("t" "todo" entry (file (concat org-directory "/gtd.org"))
+;; 	   "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+;; 	  ("n" "note" entry (file (concat org-directory "/gtd.org"))
+;; 	   "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+;; 	  ("j" "Journal" entry (file+datetree (concat org-directory "/diary.org"))
+;; 	   "* %?\n%U\n" :clock-in t :clock-resume t)
+;; 	  )))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; material theme
@@ -185,6 +193,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Elpy
 
+(require 'elpy)
 (elpy-enable)
 
 (custom-set-variables
@@ -200,19 +209,11 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     ("93f9654f91d31e9a9ec6ea2fcffcfcab38353a9588673f2b750e591f704cd633" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
  '(elpy-rpc-python-command "python3")
  '(global-font-lock-mode t)
- '(python-shell-interpreter "python3")
- '(pyvenv-workon "video_feedback"))
+ '(python-shell-interpreter "python3"))
 (setq elpy-rpc-backend "jedi")
 
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:foreground "white"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "chartreuse3")))))
-(put 'narrow-to-page 'disabled nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; flycheck
 
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
@@ -220,8 +221,14 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 (setq flycheck-check-syntax-automatically '(save mode-enable))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; autopep8
+
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; stolen from Clojure for the Brave and True
 
 (defvar my-packages
   '(paredit
@@ -236,16 +243,170 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 
 (add-to-list 'load-path "~/.emacs.d/customizations")
-(load "editing.el")
-(load "shell-integration.el")
+;; Customizations relating to editing a buffer.
+
+;; Highlights matching parenthesis
+(show-paren-mode 1)
+
+;; Highlight current line
+;; (global-hl-line-mode 1)
+
+;; Don't use hard tabs
+(setq-default indent-tabs-mode nil)
+
+;; When you visit a file, point goes to the last place where it
+;; was when you previously visited the same file.
+;; http://www.emacswiki.org/emacs/SavePlace
+(require 'saveplace)
+(setq-default save-place t)
+;; keep track of saved places in ~/.emacs.d/places
+(setq save-place-file (concat user-emacs-directory "places"))
+
+;; Emacs can automatically create backup files. This tells Emacs to
+;; put all backups in ~/.emacs.d/backups. More info:
+;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
+(setq auto-save-default nil)
+
+
+;; comments
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+
+;; Sets up exec-path-from shell
+;; https://github.com/purcell/exec-path-from-shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-envs
+   '("PATH")))
 
 
 ;; Hard-to-categorize customizations
-(load "misc.el")
+;; Changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+;; No need for ~ files when editing
+(setq create-lockfiles nil)
+
+;; Go straight to scratch buffer on startup
+(setq inhibit-startup-message t)
 
 ;; For editing lisps
-(load "elisp-editing.el")
-(load "setup-clojure.el")
-(load "setup-js.el")
+;; Automatically load paredit when editing a lisp file
+;; More at http://www.emacswiki.org/emacs/ParEdit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+;; eldoc-mode shows documentation in the minibuffer when writing code
+;; http://www.emacswiki.org/emacs/ElDoc
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+;;;;
+;; Clojure
+;;;;
+
+;; Enable paredit for Clojure
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+;; This is useful for working with camel-case tokens, like names of
+;; Java classes (e.g. JavaClassName)
+(add-hook 'clojure-mode-hook 'subword-mode)
+
+;; A little more syntax highlighting
+(require 'clojure-mode-extra-font-locking)
+
+;; syntax hilighting for midje
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (setq inferior-lisp-program "lein repl")
+            (font-lock-add-keywords
+             nil
+             '(("(\\(facts?\\)"
+                (1 font-lock-keyword-face))
+               ("(\\(background?\\)"
+                (1 font-lock-keyword-face))))
+            (define-clojure-indent (fact 1))
+            (define-clojure-indent (facts 1))))
+
+;;;;
+;; Cider
+;;;;
+
+;; provides minibuffer documentation for the code you're typing into the repl
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+;; go right to the REPL buffer when it's finished connecting
+(setq cider-repl-pop-to-buffer-on-connect t)
+
+;; When there's a cider error, show its buffer and switch to it
+(setq cider-show-error-buffer t)
+(setq cider-auto-select-error-buffer t)
+
+;; Where to store the cider history.
+(setq cider-repl-history-file "~/.emacs.d/cider-history")
+
+;; Wrap when navigating history.
+(setq cider-repl-wrap-history t)
+
+;; enable paredit in your REPL
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+;; Use clojure mode for other extensions
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
+
+
+;; key bindings
+;; these help me out with the way I usually develop web apps
+(defun cider-start-http-server ()
+  (interactive)
+  (cider-load-current-buffer)
+  (let ((ns (cider-current-ns)))
+    (cider-repl-set-ns ns)
+    (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
+    (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
+
+
+(defun cider-refresh ()
+  (interactive)
+  (cider-interactive-eval (format "(user/reset)")))
+
+(defun cider-user-ns ()
+  (interactive)
+  (cider-repl-set-ns "user"))
+
+(eval-after-load 'cider
+  '(progn
+     (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
+     (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
+     (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
+     (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
 
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "white"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "chartreuse3")))))
+(put 'narrow-to-page 'disabled nil)
